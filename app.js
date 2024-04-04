34ae75069f1cf3946e3e7d769ce6cdaad7dd4ac1
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-04-04 13:33:50 +0300

    REST APIs, Clients & CORS Errors

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(express.urlencoded()); // x-www-form-urlencoded <form>
 app.use(express.json()); // application/json
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/feed', feedRoutes);
 
 const port = process.env.PORT || 8000;
