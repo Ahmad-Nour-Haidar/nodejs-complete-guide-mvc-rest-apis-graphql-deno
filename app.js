177f627e426de6c9d50cbf5b7db3a2f46c10bbbb
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-04-04 13:24:11 +0300

    Sending Requests & Responses and Working with Postman

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+app.use(express.urlencoded()); // x-www-form-urlencoded <form>
+app.use(express.json()); // application/json
+
 app.use('/feed', feedRoutes);
 
 const port = process.env.PORT || 8000;
