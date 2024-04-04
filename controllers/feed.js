2daf86147f256a2c0168ed01eeafe9cc4b898c68
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-04-04 22:55:57 +0300

    Connecting Posts & Users

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,6 +3,7 @@ const path = require('path');
 const {validationResult} = require('express-validator');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -34,7 +35,6 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
-    console.log(errors.array());
     if (!errors.isEmpty()) {
         const error = new Error('Validation failed, entered data is incorrect.');
         error.statusCode = 422;
@@ -48,17 +48,27 @@ exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
+    let creator;
     const post = new Post({
         title: title,
         content: content,
         imageUrl: imageUrl,
-        creator: {name: 'Ahmad'}
+        creator: req.userId
     });
     post.save()
         .then(result => {
+            return User.findById(req.userId);
+        })
+        .then(user => {
+            creator = user;
+            user.posts.push(post);
+            return user.save();
+        })
+        .then(result => {
             res.status(201).json({
                 message: 'Post created successfully!',
-                post: result
+                post: post,
+                creator: {_id: creator._id, name: creator.name}
             });
         })
         .catch(err => {
--- models/post.js
+++ models/post.js
@@ -16,8 +16,9 @@ const postSchema = new Schema(
             required: true
         },
         creator: {
-            type: Object,
-            required: String
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            require: true,
         }
     },
     {timestamps: true}
