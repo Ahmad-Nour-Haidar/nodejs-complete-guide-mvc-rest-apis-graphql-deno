2f88e34f5668b591f3c750fe407fc2c410230511
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-04-04 23:50:37 +0300

    Transforming Then Catch to Async Await

--- controllers/feed.js
+++ controllers/feed.js
@@ -5,32 +5,24 @@ const {validationResult} = require('express-validator');
 const Post = require('../models/post');
 const User = require('../models/user');
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
     const currentPage = req.query.page || 1;
     const perPage = 2;
-    let totalItems;
-    Post.find()
-        .countDocuments()
-        .then(count => {
-            totalItems = count;
-            return Post.find()
-                .skip((currentPage - 1) * perPage)
-                .limit(perPage);
-        })
-        .then(posts => {
-            res.status(200)
-                .json({
-                    message: 'Fetched posts successfully.',
-                    posts: posts,
-                    totalItems: totalItems
-                });
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+    try {
+        const totalItems = await Post.find().countDocuments();
+        const posts = await Post.find().skip((currentPage - 1) * perPage).limit(perPage);
+        res.status(200)
+            .json({
+                message: 'Fetched posts successfully.',
+                posts: posts,
+                totalItems: totalItems,
+            });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
 };
 
 exports.createPost = (req, res, next) => {
@@ -50,10 +42,7 @@ exports.createPost = (req, res, next) => {
     const content = req.body.content;
     let creator;
     const post = new Post({
-        title: title,
-        content: content,
-        imageUrl: imageUrl,
-        creator: req.userId
+        title: title, content: content, imageUrl: imageUrl, creator: req.userId
     });
     post.save()
         .then(result => {
@@ -66,9 +55,7 @@ exports.createPost = (req, res, next) => {
         })
         .then(result => {
             res.status(201).json({
-                message: 'Post created successfully!',
-                post: post,
-                creator: {_id: creator._id, name: creator.name}
+                message: 'Post created successfully!', post: post, creator: {_id: creator._id, name: creator.name}
             });
         })
         .catch(err => {
