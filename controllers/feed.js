ed48ed02138d0ae72a9a1687014d90fb739b3078
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-04-06 12:46:25 +0300

    Updating Posts On All Connected Clients

--- controllers/feed.js
+++ controllers/feed.js
@@ -118,13 +118,15 @@ exports.updatePost = async (req, res, next) => {
         const postId = req.params.postId;
         const title = req.body.title;
         const content = req.body.content;
-        const post = await Post.findById(postId);
+        const post = await Post.findById(postId).populate('creator');
         if (!post) {
             const error = new Error('Could not find post.');
             error.statusCode = 404;
             throw error;
         }
-        if (post.creator.toString() !== req.userId) {
+        console.log(post.creator._id.toString());
+        console.log(req.userId);
+        if (post.creator._id.toString() !== req.userId) {
             const error = new Error('Not authorized!, can not edit.');
             error.statusCode = 403;
             throw error;
@@ -143,6 +145,7 @@ exports.updatePost = async (req, res, next) => {
         post.content = content;
         post.imageUrl = imageUrl;
         const result = await post.save();
+        io.getIO().emit('posts', {action: 'update', post: result});
         res.status(200)
             .json({
                 message: 'Post updated!',
