0f5a466d76ae9fe8ec30e14c007c6bef8d9cd179
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-04-07 09:51:12 +0300

    Adding a Get Post Query & Resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -109,4 +109,27 @@ module.exports = {
             updatedAt: createdPost.updatedAt.toISOString()
         };
     },
+
+    posts: async function ({page}, req) {
+        if (!req.isAuth) {
+            const error = new Error('Not authenticated!');
+            error.code = 401;
+            throw error;
+        }
+        const totalPosts = await Post.find().countDocuments();
+        const posts = await Post.find()
+            .sort({createdAt: -1})
+            .populate('creator');
+        return {
+            posts: posts.map(p => {
+                return {
+                    ...p._doc,
+                    _id: p._id.toString(),
+                    createdAt: p.createdAt.toISOString(),
+                    updatedAt: p.updatedAt.toISOString()
+                };
+            }),
+            totalPosts: totalPosts
+        };
+    }
 };
