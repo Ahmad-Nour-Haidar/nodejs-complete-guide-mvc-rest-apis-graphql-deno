522b816fa1534f64e4ba43a8a8f345c815301d7a
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-04-07 11:54:10 +0300

    Managing the User Status

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -233,5 +233,37 @@ module.exports = {
         user.posts.pull(id);
         await user.save();
         return true;
+    },
+
+    user: async function (args, req) {
+        if (!req.isAuth) {
+            const error = new Error('Not authenticated!');
+            error.code = 401;
+            throw error;
+        }
+        const user = await User.findById(req.userId);
+        if (!user) {
+            const error = new Error('No user found!');
+            error.code = 404;
+            throw error;
+        }
+        return {...user._doc, _id: user._id.toString()};
+    },
+
+    updateStatus: async function ({status}, req) {
+        if (!req.isAuth) {
+            const error = new Error('Not authenticated!');
+            error.code = 401;
+            throw error;
+        }
+        const user = await User.findById(req.userId);
+        if (!user) {
+            const error = new Error('No user found!');
+            error.code = 404;
+            throw error;
+        }
+        user.status = status;
+        await user.save();
+        return {...user._doc, _id: user._id.toString()};
     }
 };
