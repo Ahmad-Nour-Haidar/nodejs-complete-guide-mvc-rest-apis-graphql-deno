26d0e4fc6c5f8d299f06c107bb039daf4b2340cb
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-04-07 11:10:27 +0300

    Updating Posts

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -157,5 +157,55 @@ module.exports = {
             createdAt: post.createdAt.toISOString(),
             updatedAt: post.updatedAt.toISOString()
         };
-    }
+    },
+
+    updatePost: async function ({id, postInput}, req) {
+        if (!req.isAuth) {
+            const error = new Error('Not authenticated!');
+            error.code = 401;
+            throw error;
+        }
+        const post = await Post.findById(id).populate('creator');
+        if (!post) {
+            const error = new Error('No post found!');
+            error.code = 404;
+            throw error;
+        }
+        if (post.creator._id.toString() !== req.userId.toString()) {
+            const error = new Error('Not authorized!');
+            error.code = 403;
+            throw error;
+        }
+        const errors = [];
+        if (
+            validator.isEmpty(postInput.title) ||
+            !validator.isLength(postInput.title, {min: 5})
+        ) {
+            errors.push({message: 'Title is invalid.'});
+        }
+        if (
+            validator.isEmpty(postInput.content) ||
+            !validator.isLength(postInput.content, {min: 5})
+        ) {
+            errors.push({message: 'Content is invalid.'});
+        }
+        if (errors.length > 0) {
+            const error = new Error('Invalid input.');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
+        post.title = postInput.title;
+        post.content = postInput.content;
+        if (postInput.imageUrl !== 'undefined') {
+            post.imageUrl = postInput.imageUrl;
+        }
+        const updatedPost = await post.save();
+        return {
+            ...updatedPost._doc,
+            _id: updatedPost._id.toString(),
+            createdAt: updatedPost.createdAt.toISOString(),
+            updatedAt: updatedPost.updatedAt.toISOString()
+        };
+    },
 };
